a585ead690da50d342a0c23203e6b0890db4b862
Felipe Franceschini <92216782+Bert00100@users.noreply.github.com>
2025-11-09 19:45:43 +0000

    Mensagem para Luiza

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        Lu, me manda um audio me falando como se pronuncia <span>RISE BIT</span>
+      </h1>
+    </div>
+  );
+}
