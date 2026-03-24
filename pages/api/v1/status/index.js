bf5f702ffa158849f53b764e4de521ad7abf47ea
Felipe Bertoncini <felipe.bertoncini@mettric.com.br>
2026-03-24 12:59:32 -0300

    Adicao de ´data_base_version´ e ´max_connect´;

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,23 @@
 import database from "infra/database.js";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ servidor: "Online" });
+  const udpdateAt = new Date().toISOString();
+
+  //Versão do Postgres
+  const dbVersionValue = await database.query("SELECT version();");
+  const dbVersionResult = dbVersionValue.rows[0].version.split(" ")[1];
+  //Conexẽs Maximas
+  const dbMaxConnectionValue = await database.query("SHOW max_connections;")
+  const dbMaxConnectionResult = dbMaxConnectionValue.rows[0].max_connections;
+  //Conexões Usadas
+  response.status(200).json({
+    update_at: udpdateAt,
+    database:{
+      data_base_version: dbVersionResult,
+      max_connect: dbMaxConnectionResult,
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+
+  const parseUpdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parseUpdateAt);
+
+  expect(responseBody.database.data_base_version).toEqual("18.0");
+  expect(responseBody.database.max_connect).toBe('100');
 });
