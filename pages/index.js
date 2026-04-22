20f76e9d72e51c4705f3b1392eee321ba40d84a9
Bert00100 <felipebertoncinifranceschini@gmail.com>
2026-04-22 18:42:41 -0300

    Alterações do claude para seguir o padrão do curso

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>
-        Lu, me manda um audio me falando como se pronuncia <span>RISE BIT</span>
-      </h1>
-    </div>
-  );
+  return <div />;
 }
